a28dcb1e14c8b80644726734a843b6eeb7089cf3
Adrian-ortiz0 <dxniel7328@gmail.com>
2024-08-19 15:19:02 -0500

    feat: :sparkles: I create the js DOM manipulation

---
+++ main.js
@@ -0,0 +1,34 @@
+const cabeceras = new Headers();
+cabeceras.set("Content-Type", "application/json");
+cabeceras.set("Content-Encoding", "br");
+
+async function peticion(url) {
+  const respuesta = await fetch(url);
+  if (respuesta.ok) {
+    const info = await respuesta.json();
+    return info;
+  } else {
+    return [];
+  }
+}
+
+const btnPersonajes = document.getElementById("personajesBtn");
+const textoInicial = document.querySelector(".texto_inicial");
+
+btnPersonajes.onclick = traerPersonajes;
+
+let urlPersonajes = "https://swapi.py4e.com/api/people";
+
+async function traerPersonajes() {
+  const contenedor = document.querySelector(".contenedor");
+  const personajes = await peticion(urlPersonajes);
+  const listaPersonajes = personajes.results;
+  textoInicial.classList.add("texto_oculto");
+
+  listaPersonajes.forEach(function (personaje) {
+    const texto = document.createElement("p");
+    texto.classList.add("personajes_list");
+    texto.innerHTML = `Nombre: ${personaje.name} | Altura: ${personaje.height} | Peso: ${personaje.mass}`;
+    contenedor.appendChild(texto);
+  });
+}
